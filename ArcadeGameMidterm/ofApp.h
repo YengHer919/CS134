beea809769a494f59b86a8481fa6b3d3d58f0b02
Yeng Her <119150603+YengHer919@users.noreply.github.com>
2025-03-24 16:11:44 -0700

    Update ofApp.h

--- ArcadeGameMidterm/ofApp.h
+++ ArcadeGameMidterm/ofApp.h
@@ -9,14 +9,14 @@ class Agent : public Sprite {
 public:
 	Agent() {
 		Sprite::Sprite();
-//		cout << "in Agent Constuctor" << endl;
+		//		cout << "in Agent Constuctor" << endl;
 	}
 };
 
 class AgentEmitter : public Emitter {
 public:
 	void AgentEmitter::spawnSprite() {
-//		cout << "in AgentEmitter::spawnSprite" << endl;
+		//		cout << "in AgentEmitter::spawnSprite" << endl;
 		Agent sprite;
 		sprite.velocity = glm::normalize(velocity) * 300.0f;
 		sprite.lifespan = lifespan;
@@ -24,80 +24,56 @@ public:
 		sprite.birthtime = ofGetElapsedTimeMillis();
 		sys->add(sprite);
 	}
-	void moveSprite(Sprite *sprite) {
+	void moveSprite(Sprite* sprite) {
 		Emitter::moveSprite(sprite);
 		glm::vec3 v = glm::normalize(sprite->velocity);
 		glm::vec3 u = glm::vec3(0, -1, 0);
 		float a = glm::orientedAngle(u, v, glm::vec3(0, 0, 1));
-		sprite->rot =  glm::degrees(a);
+		sprite->rot = glm::degrees(a);
 	}
 };
 
-class Pillar : public Shape {
+class ofApp : public ofBaseApp {
+
 public:
-	Pillar() {
-		width = 30;
-		height = 100;
-		lifespan = 5000; // 5 seconds in milliseconds
-		birthtime = ofGetElapsedTimeMillis();
-		color = ofColor::red;
-	}
+	void setup();
+	void update();
+	void draw();
 
-	void draw() {
-		ofPushMatrix();
-		ofMultMatrix(getTransform());
-		ofSetColor(color);
-		ofDrawRectangle(-width / 2, -height / 2, width, height);
-		ofPopMatrix();
-	}
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	void moveForward();
+	void moveBackward();
+	void spawnPillar();
+	glm::vec3 heading(Emitter* emitter);
 
-	bool isAlive() {
-		return (ofGetElapsedTimeMillis() - birthtime < lifespan);
-	}
+	Emitter* emitter = NULL;
 
-	float width, height;
-	float lifespan;
-	uint64_t birthtime;
-	ofColor color;
-};
-
-class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		void moveForward();
-		void moveBackward();
-		void spawnPillar();
-		glm::vec3 heading(Emitter* emitter);
-
-		Emitter  *emitter = NULL;
-		
-		ofVec3f mouse_last;
-		bool imageLoaded;
-		bool up;
-		bool down;
-		bool left;
-		bool right;
-		float rate = 5;
-		float life = 5;
-		float velocity = 100;
-		float scale = 0.3;
-		float rotationSpeed = 3.0;
-		vector<Pillar> pillars;
-		float pillarSpawnTime = 2000; // milliseconds between pillar spawns
-		uint64_t lastPillarSpawnTime = 0;
+	ofVec3f mouse_last;
+	bool imageLoaded;
+	bool up;
+	bool down;
+	bool left;
+	bool right;
+	float rate = 5;
+	float life = 5;
+	float velocity = 100;
+	float scale = 0.3;
+	float rotationSpeed = 3.0;
+	vector<Pillar> pillars;
+	uint64_t lastPillarSpawnTime = 0;
+	uint64_t hit = 0;
+	bool gameOver = false;
+	vector<WarningPillar> warningPillars;
+	uint64_t nextPillarSpawnTime = 2000; // Time to spawn the next pillar
+	int difficulty = 0;
 };
